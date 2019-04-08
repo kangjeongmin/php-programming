ca6068af42e24aab2267148c83f13cf71e784475
kangjeongmin <jmin1018@naver.com>
2019-04-08 21:33:54 +0900

    Update index.php

--- 과제/index.php
+++ 과제/index.php
@@ -1,8 +1,13 @@
 <?php
     # TODO: MySQL 데이터베이스 연결 및 레코드 가져오기!
+    $connect = mysql_connect("localhost", "kjm", "1234");  // MySQL 데이터베이스 연결
+    mysql_select_db("kjm_db", $connect); // DB 선택
+    $sql = "select * from tableboard_shop";  // sql 쿼리 string 생성
+    $result = mysql_query($sql);   // sql 쿼리 실행
+    $numOfrow = @mysql_num_rows($result);   // 선택된 sql 갯수
+
 ?>
 
-<!-- 출처 : https://colorlib.com/wp/template/responsive-table-v1/ -->
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -48,120 +53,18 @@
                     <?php
                         # TODO : 아래 표시되는 내용을, MySQL 테이블에 있는 레코드로 대체하기!
                         # Note : column6 에 해당하는 Total 은 Price 값과 Quantity 값의 곱으로 표시!
+                        for($i=0;$i<$numOfrow;$i++) {
+                            $row = @mysql_fetch_row($result);
+                            echo '<tr onclick="location.href = (\'board_form.php?num='; echo $i+1;echo '\')">';
+                                echo '<td class="column1">'; echo $row[1]; echo '</td>';
+                                echo '<td class="column2">'; echo $row[2]; echo '</td>';
+                                echo '<td class="column3">'; echo $row[3]; echo '</td>';
+                                echo '<td class="column4">'; echo '$'; echo $row[4]; echo '</td>';
+                                echo '<td class="column5">'; echo $row[5]; echo '</td>';
+                                echo '<td class="column6">'; echo '$'; echo $row[4]*$row[5]; echo '</td>';
+                            echo '</tr>';
+                        }
                     ?>
-                    <tr onclick="location.href = ('board_form.php?num=0')">
-                        <td class="column1">2017-09-29 01:22</td>
-                        <td class="column2">200398</td>
-                        <td class="column3">iPhone X 64Gb Grey</td>
-                        <td class="column4">$999.00</td>
-                        <td class="column5">1</td>
-                        <td class="column6">$999.00</td>
-                    </tr>
-                    <tr onclick="location.href = ('board_form.php?num=1')">
-                        <td class="column1">2017-09-28 05:57</td>
-                        <td class="column2">200397</td>
-                        <td class="column3">Samsung S8 Black</td>
-                        <td class="column4">$756.00</td>
-                        <td class="column5">1</td>
-                        <td class="column6">$756.00</td>
-                    </tr>
-                    <tr onclick="location.href = ('board_form.php?num=2')">
-                        <td class="column1">2017-09-26 05:57</td>
-                        <td class="column2">200396</td>
-                        <td class="column3">Game Console Controller</td>
-                        <td class="column4">$22.00</td>
-                        <td class="column5">2</td>
-                        <td class="column6">$44.00</td>
-                    </tr>
-                    <tr onclick="location.href = ('board_form.php?num=3')">
-                        <td class="column1">2017-09-25 23:06</td>
-                        <td class="column2">200392</td>
-                        <td class="column3">USB 3.0 Cable</td>
-                        <td class="column4">$10.00</td>
-                        <td class="column5">3</td>
-                        <td class="column6">$30.00</td>
-                    </tr>
-                    <tr onclick="location.href = ('board_form.php?num=4')">
-                        <td class="column1">2017-09-24 05:57</td>
-                        <td class="column2">200391</td>
-                        <td class="column3">Smartwatch 4.0 LTE Wifi</td>
-                        <td class="column4">$199.00</td>
-                        <td class="column5">6</td>
-                        <td class="column6">$1494.00</td>
-                    </tr>
-                    <tr onclick="location.href = ('board_form.php?num=5')">
-                        <td class="column1">2017-09-23 05:57</td>
-                        <td class="column2">200390</td>
-                        <td class="column3">Camera C430W 4k</td>
-                        <td class="column4">$699.00</td>
-                        <td class="column5">1</td>
-                        <td class="column6">$699.00</td>
-                    </tr>
-                    <tr onclick="location.href = ('board_form.php?num=6')">
-                        <td class="column1">2017-09-22 05:57</td>
-                        <td class="column2">200389</td>
-                        <td class="column3">Macbook Pro Retina 2017</td>
-                        <td class="column4">$2199.00</td>
-                        <td class="column5">1</td>
-                        <td class="column6">$2199.00</td>
-                    </tr>
-                    <tr onclick="location.href = ('board_form.php?num=7')">
-                        <td class="column1">2017-09-21 05:57</td>
-                        <td class="column2">200388</td>
-                        <td class="column3">Game Console Controller</td>
-                        <td class="column4">$999.00</td>
-                        <td class="column5">1</td>
-                        <td class="column6">$999.00</td>
-                    </tr>
-                    <tr onclick="location.href = ('board_form.php?num=8')">
-                        <td class="column1">2017-09-19 05:57</td>
-                        <td class="column2">200387</td>
-                        <td class="column3">iPhone X 64Gb Grey</td>
-                        <td class="column4">$999.00</td>
-                        <td class="column5">1</td>
-                        <td class="column6">$999.00</td>
-                    </tr>
-                    <tr onclick="location.href = ('board_form.php?num=9')">
-                        <td class="column1">2017-09-18 05:57</td>
-                        <td class="column2">200386</td>
-                        <td class="column3">iPhone X 64Gb Grey</td>
-                        <td class="column4">$999.00</td>
-                        <td class="column5">1</td>
-                        <td class="column6">$999.00</td>
-                    </tr>
-                    <tr onclick="location.href = ('board_form.php?num=10')">
-                        <td class="column1">2017-09-22 05:57</td>
-                        <td class="column2">200389</td>
-                        <td class="column3">Macbook Pro Retina 2017</td>
-                        <td class="column4">$2199.00</td>
-                        <td class="column5">1</td>
-                        <td class="column6">$2199.00</td>
-                    </tr>
-                    <tr onclick="location.href = ('board_form.php?num=11')">
-                        <td class="column1">2017-09-21 05:57</td>
-                        <td class="column2">200388</td>
-                        <td class="column3">Game Console Controller</td>
-                        <td class="column4">$999.00</td>
-                        <td class="column5">1</td>
-                        <td class="column6">$999.00</td>
-                    </tr>
-                    <tr onclick="location.href = ('board_form.php?num=12')">
-                        <td class="column1">2017-09-19 05:57</td>
-                        <td class="column2">200387</td>
-                        <td class="column3">iPhone X 64Gb Grey</td>
-                        <td class="column4">$999.00</td>
-                        <td class="column5">1</td>
-                        <td class="column6">$999.00</td>
-                    </tr>
-                    <tr onclick="location.href = ('board_form.php?num=13')">
-                        <td class="column1">2017-09-18 05:57</td>
-                        <td class="column2">200386</td>
-                        <td class="column3">iPhone X 64Gb Grey</td>
-                        <td class="column4">$999.00</td>
-                        <td class="column5">1</td>
-                        <td class="column6">$999.00</td>
-                    </tr>
-
                     </tbody>
                 </table>
             </div>
