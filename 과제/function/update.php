ae61244f27d8361ba5d6836d3b902bedb76589c5
kangjeongmin <jmin1018@naver.com>
2019-04-08 21:35:10 +0900

    Update update.php

--- 과제/function/update.php
+++ 과제/function/update.php
@@ -7,9 +7,17 @@
  */
 
 # TODO: MySQL DB에서, num에 해당하는 레코드를 POST로 받아온 내용으로 수정하기!
-
+$connect = mysql_connect("localhost", "kjm", "1234");
+// DB 선택
+mysql_select_db("kjm_db", $connect);
+// sql 쿼리 string 생성
+$sql = "update tableboard_shop set date ='$_POST[date]',orderid ='$_POST[order_id]', name='$_POST[name]',price='$_POST[price]',quantity='$_POST[quantity]' where num = $_GET[num]";
+// sql 쿼리 실행
+$result = mysql_query($sql);
 # 참고 : 에러 메시지 출력 방법
-echo "<script> alert('update - error message') </script>"
+if(!$result) {
+    echo "<script> alert('update - error message') </script>";
+}
 
 ?>
 
