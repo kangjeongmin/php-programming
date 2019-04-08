aa4730aa012777fd2780e5d59ab7c56ac3210f0b
kangjeongmin <jmin1018@naver.com>
2019-04-08 21:34:51 +0900

    Update insert.php

--- 과제/function/insert.php
+++ 과제/function/insert.php
@@ -7,9 +7,19 @@
  */
 
 # TODO: MySQL DB에서, POST로 받아온 내용 입력하기!
-
+$connect = mysql_connect("localhost", "kjm", "1234");
+// DB 선택
+mysql_select_db("kjm_db", $connect);
+// sql 쿼리 string 생성
+$sql = "insert into tableboard_shop(date,orderid, name, price, quantity) 
+             values('$_POST[date]','$_POST[order_id]','$_POST[name]','$_POST[price]','$_POST[quantity]')";
+// sql 쿼리 실행
+$result = mysql_query($sql);
 # 참고 : 에러 메시지 출력 방법
-echo "<script> alert('insert - error message') </script>"
+if(!$result) {
+    echo "<script> alert('ERROR!!'); </script>";
+}
+mysql_close();
 
 ?>
 
